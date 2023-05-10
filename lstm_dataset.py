6d2f097a86c61bd91b1d6c48fb342bacad01488b
eichzz <hectoramm.rubio@gmail.com>
2023-05-10 22:11:01 +0200

    lstm model

--- lstm_dataset.py
+++ lstm_dataset.py
@@ -2,45 +2,59 @@ import numpy as np
 import scipy.io
 import tensorflow as tf
 import pandas as pd
+import sklearn as skl
+import matplotlib.pyplot as plt
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
+from sklearn.model_selection import train_test_split
 
-# Convert dictionary to Pandas DataFrame
-df = pd.read_excel('2015_30min.xlsx', header= None)[0]
+# Read the data and convert dictionary to Pandas DataFrame
+df = pd.read_excel(
+    "C:/Users/hecto/Documents/Master/tfm/tfm-renewable-energy-deep-learning/2015_30min.xlsx",
+    header=None)[0]
 
-# Data preprocessing: each row will contain the 20 day measures, and the 20 measures 
-# who will be predicted for the next day by the algorithm.
-print("Num rows: " + str(df.shape[0]))
-numRows = int(df.shape[0] / 10)
+# Data preprocessing: each row will contain the 10 measures for each day , and the 10 measures for the following day
+X = pd.DataFrame(np.array(df).reshape(-1, 10), columns=["col_{}".format(i) for i in range(0, 10)])
 
-dfMulti = pd.DataFrame(columns=['col' + str(i) for i in range(0, 9)])
+y = pd.DataFrame.copy(X)
 
-for i in range(numRows):
-    start = 10 * i
-    end = start + 10
-    print("Start: " + str(start) + ". End: " + str(end) + ". " + str(df.iloc[start:end]))
-    dfMulti.loc[i] = df.iloc[start:end].values.flatten()
+y.columns = ["col_{}".format(i) for i in range(11, 21)]
+y = y.drop(0)
+y = y.reset_index(drop=True)
+y.loc[len(y)] = np.zeros(10)
 
-print(dfMulti)
+dfPreproccessed = pd.concat([X, y], axis=1)
 
+print(dfPreproccessed)
 
-# target = 
-# define the model
-# model = Sequential()
-# model.add(LSTM(32, input_shape=(10, 1)))
-# model.add(Dense(1, activation='sigmoid'))
-
-# # compile the model
-# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# train the model with GPU acceleration
-# with tf.device('/GPU:0'):
-#   model.fit(data, target, epochs=10, batch_size=32)
+# Split the data into training and validation sets
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, random_state=42)
 
 
-# In this example, we first generate some sample data where each sample is a sequence of 10 time steps with one feature. 
-# We then define an LSTM model with 32 units and a dense layer with a sigmoid activation function.
-# We compile the model using binary cross-entropy loss and the Adam optimizer, and then train the model on the sample data for 10 epochs with a batch size of 32.
-# The with tf.device('/GPU:0'): statement specifies that the model training should be run on the first available GPU. 
-# This ensures that the training is accelerated using GPU hardware.
+# Model definition
+model = Sequential()
+model.add(LSTM(32, input_shape=(X_train.shape[1], 1)))
+model.add(Dense(1, activation="sigmoid"))
+
+# Model compilation
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+# Model training with GPU acceleration
+with tf.device("/GPU:0"):
+    history = model.fit(
+        X_train.reshape((X_train.shape[0], X_train.shape[1], 1)),
+        y_train,
+        epochs=10,
+        batch_size=16,
+        validation_data=(X_val.reshape((X_val.shape[0], X_val.shape[1], 1)), y_val))
+
+# Training and Validation loss curves
+plt.plot(history.history['loss'], label='Training loss')
+plt.plot(history.history['val_loss'], label='Validation loss')
+plt.legend()
+plt.show()
+
+# Model evaluation with validation data
+score = model.evaluate(X_val.reshape((X_val.shape[0], X_val.shape[1], 1)), y_val)
+print('Validation loss:', score)
